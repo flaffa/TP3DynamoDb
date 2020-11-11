e8ba9b6296c8d5a798e7ab029d63b7d32f5b2292
flaffa <70919669+flaffa@users.noreply.github.com>
2020-11-11 15:52:56 -0300

    Add files via upload

---
+++ creat table.js
@@ -0,0 +1,50 @@
+var params = {
+    TableName: 'Envio',
+    KeySchema: [
+        { 
+            AttributeName: 'id',
+            KeyType: 'HASH',
+        }
+    ],
+    AttributeDefinitions: [
+        {
+            AttributeName: 'id',
+            AttributeType: 'S'
+        },
+        {
+            AttributeName: 'pendiente',
+            AttributeType: 'S'
+        }
+    ],
+    ProvisionedThroughput: { 
+        ReadCapacityUnits: 1, 
+        WriteCapacityUnits: 1, 
+    },
+    GlobalSecondaryIndexes: [
+        { 
+            IndexName: 'EnviosPendientesIndex', 
+            KeySchema: [
+                { 
+                    AttributeName: 'id',
+                    KeyType: 'HASH',
+                },
+                { 
+                    AttributeName: 'pendiente', 
+                    KeyType: 'RANGE', 
+                }
+            ],
+            Projection: {
+                ProjectionType: 'KEYS_ONLY'
+            },
+            ProvisionedThroughput: { 
+                ReadCapacityUnits: 1,
+                WriteCapacityUnits: 1,
+            },
+        },
+    ],
+};
+dynamodb.createTable(params, function(err, data) {
+    if (err) ppJson(err); // an error occurred
+    else ppJson(data); // successful response
+
+});
